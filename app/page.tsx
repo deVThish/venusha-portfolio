036afee6f3a48769cbedc4e48fab984bbd1b9075
deVThish <277204864+deVThish@users.noreply.github.com>
2026-07-18 13:55:29 +0530

    Contract page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Contact from "./components/Contact";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Projects />
       <Skills />
+      <Contact />
     </main>
   );
 }
